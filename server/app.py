492cfe057404f4a64ad20345afeb7d05a11d0851
tdsk68 <daisuke.bfb@gmail.com>
2023-10-29 22:29:36 +0900

    POST API作成

--- server/app.py
+++ server/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
@@ -6,3 +6,19 @@ app = Flask(__name__)
 def index():
     return "<p>index page</p>"
 
+@app.route("/greeting", methods=['GET'])
+def greeting_get():
+    return jsonify({"message": "Let's try greeting"})
+
+@app.route("/greeting", methods=['POST'])
+def greeting_post():
+    data = request.get_json()
+    if 'name' in data:
+        name = data['name']
+        response = {'message': 'hello ' + name + '!'}
+    else:
+        response = {'message': 'none'}
+    return jsonify(response)
+
+if __name__ == '__main__':
+    app.run(debug=True)
